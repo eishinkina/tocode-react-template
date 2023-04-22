ee1587a6e9222f1fbf7f640821d4aaca4f13423a
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-04-22 15:47:27 +0400

    Making a dynamic menu

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Header, Footer } from "./components";
 import "./scss/main.scss";
 import Container from "./layouts/Container";
+import { Routes, Route } from "react-router-dom";
 
 
 const App = () => {
@@ -10,7 +11,10 @@ const App = () => {
       <Header isLogo />
       <div className="ui-content-wrapper">
         <Container>
-          <h1>template</h1>
+          <Routes>
+            <Route path="/" element={<h1>Home page</h1>} />
+            <Route path="/about" element={<h1>About page</h1>} />
+          </Routes>
         </Container>
       </div>
       <Footer />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,44 +1,62 @@
-import React from "react";
-import Container from "../../layouts/Container";
-import "./../../scss/main.scss";
-import propTypes from "prop-types";
-import classNames from "classnames";
+import React from 'react'
+import Container from '../../layouts/Container'
+import './../../scss/main.scss'
+import propTypes from 'prop-types'
+import classNames from 'classnames'
+import "./Header.scss"
 
 //icons
-import { ReactComponent as LogoIcon } from "../../assets/img/logo.svg";
+import { ReactComponent as LogoIcon } from '../../assets/img/logo.svg'
+import { NavLink } from 'react-router-dom'
+
+const menuLinks = [
+  {
+    title: 'Home',
+    alias: '/',
+  },
+  {
+    title: 'About',
+    alias: '/about',
+  },
+]
+
+const menuItems = menuLinks.map((item) => (
+  <li key={item.alias}>
+    <NavLink to={item.alias}><div className='ui-button isLink'>{item.title}</div></NavLink>
+  </li>
+))
+
+const menuList = <ul className='HeaderList'>{menuItems}</ul>
 
 function Header({ isLogo, isFixed, className, ...attrs }) {
   const classes = classNames(className, {
     isFixed,
-  });
+  })
   // console.log('hello')
   return (
-    <header className={classes}{...attrs}>
+    <header className={classes} {...attrs}>
       <Container>
-        <div className="Header flex justify-between py-2 mb-4">
-          <div className="Logo">
+        <div className='Header flex justify-between py-2 mb-4'>
+          <div className='Logo'>
             {isLogo && <LogoIcon />}
             <span>React template </span>
           </div>
-          <ul className="ui-button-group">
-            <li className="ui-link">Home</li>
-            <li className="ui-link">About</li>
-          </ul>
+          {menuList}
         </div>
       </Container>
     </header>
-  );
+  )
 }
 
 Header.prototype = {
   isLogo: propTypes.bool,
   isFixed: propTypes.bool,
   className: propTypes.string,
-};
+}
 Header.defaultProps = {
   isLogo: false,
   isFixed: false,
-  className: "", 
-};
+  className: '',
+}
 
-export default Header;
+export default Header
